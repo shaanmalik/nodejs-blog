31cbb460c13abbecee589a831bbd06dc854b2097
Shaan Malik <shaanmalik@gmail.com>
2016-05-15 11:32:29 +0100

    Rendering homepage using handlebars template + model data.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-var express = require('express');
+var fs = require('fs');
 
+var express = require('express');
+var handlebars = require('handlebars');
 var app = express();
+
+var homepage = fs.readFileSync('templates/homepage.hbs').toString();
+var homepageTemplate = handlebars.compile(homepage);
 
 app.use(express.static('html-mockup'));
 
+var model =
+{
+    'blog': {
+        'title':'Mah Blog'
+    },
+    'posts': [
+        {
+            'date': '2/1/12',
+            'title': 'Blog entry #1',
+            'content': "Hello world!",
+            'tags': ['Funny']
+        },
+        {
+            'date': '2/1/12',
+            'title': 'Blog entry #1',
+            'content': "Hello world!",
+            'tags': ['Funny']
+        }
+    ]
+};
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+    var html = homepageTemplate(model);
+    res.send(html);
 });
 
 app.listen(3000, function() {
